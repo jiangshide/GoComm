7ace91ddd86ca8213b67eacb777c1eb8b97b4d81
jankey <792868+jiangshide@users.noreply.github.com>
2020-07-07 12:26:06 +0800

    Update string.go

--- utils/string.go
+++ utils/string.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"time"
 	"unicode/utf8"
+	"bytes"
+	"math"
 )
 
 var emailPattern = regexp.MustCompile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[a-zA-Z0-9](?:[\\w-]*[\\w])?")
@@ -195,4 +197,61 @@ func GetRandomName() string {
     }
     //返回姓名
     return fmt.Sprintf("%s%s", fmt.Sprint(lastName[rand.Intn(lastNameLen-1)]), first)
+}
+
+/**
+* @des 时间转换函数
+* @param atime string 要转换的时间戳（秒）
+* @return string
+*/
+func StrTime (atime int64) string{
+    var byTime = []int64{365*24*60*60,24*60*60,60*60,60,1}
+    var unit = []string{"年前","天前","小时前","分钟前","秒钟前"}
+    now := time.Now().Unix()
+    ct := now - atime
+    if ct < 0{
+        return "刚刚"
+    }
+    var res string
+    for i := 0;i < len(byTime);i++{
+        if ct < byTime[i]{
+            continue
+        }
+        var temp = math.Floor(float64(ct / byTime[i]))
+        ct = ct % byTime[i];
+        if temp > 0 {
+            var tempStr string
+            tempStr = strconv.FormatFloat(temp,'f',-1,64)
+            res = MergeString(tempStr,unit[i]) //此处调用了一个我自己封装的字符串拼接的函数（你也可以自己实现）
+        }
+        break//我想要的形式是精确到最大单位，即："2天前"这种形式，如果想要"2天12小时36分钟48秒前"这种形式，把此处break去掉，然后把字符串拼接调整下即可（别问我怎么调整，这如果都不会我也是无语）
+    }
+    return res
+}
+
+/**
+* @des 拼接字符串
+* @param args ...string 要被拼接的字符串序列
+* @return string
+*/
+func MergeString (args ...string) string {
+    buffer := bytes.Buffer{}
+    for i:=0; i<len(args); i++ {
+        buffer.WriteString(args[i])
+    }
+    return buffer.String()
+}
+
+func ShowNum(str string)string{
+	num,_ := strconv.Atoi(str);
+	if num >= 100000000{
+		return strconv.Itoa(num/100000000.0)+"亿"
+	}
+	if num >= 10000 {
+		return strconv.Itoa(num/10000.0) +"万"		
+	}
+	if num >= 1000 {
+		return strconv.Itoa(num/1000.0)+"千"
+	}
+	return strconv.Itoa(num)
 }
